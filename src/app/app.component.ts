9a37a47488f907d8a21f4f939aeea39743edd59e
Clarice Bianchini <claricebbcosta@gmail.com>
2020-06-30 00:57:57 -0300

    feat: Includes icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {CalculateService} from '../calculate.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'origin-angular';
+export class AppComponent implements OnInit, OnDestroy, AfterContentInit {
+
+  constructor(private calculateService: CalculateService) {
+  }
+
+  ngOnInit(): void {
+    console.log('oi');
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  ngAfterContentInit(): void {
+  }
 }
