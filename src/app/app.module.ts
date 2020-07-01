10246408a13ae80079d6a7084b4aeb2e04c531ae
Clarice Bianchini <claricebbcosta@gmail.com>
2020-06-30 22:25:55 -0300

    refactor: Update css classes to be more semantic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { InstallmentsFormComponent } from './ components/installments-form/installments-form.component';
+import { InstallmentsFormComponent } from './components/installments-form/installments-form.component';
 import {FormsModule} from '@angular/forms';
+import {InputMaskService} from './services/input-mask.service';
+import { SubmitButtonComponent } from './components/submit-button/submit-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InstallmentsFormComponent
+    InstallmentsFormComponent,
+    SubmitButtonComponent
   ],
   imports: [
     BrowserModule,
     ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
     FormsModule
   ],
-  providers: [],
+  providers: [InputMaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
